0eaf206ed98fbed1dfa5ed8a563a0b1c5adc8d35
BAD Gumby <badgumby@users.noreply.github.com>
2019-01-23 13:45:42 -0600

    update packages and config

--- polybar/scripts/nics.sh
+++ polybar/scripts/nics.sh
@@ -5,13 +5,13 @@ NIC=$1
 
 # Signal Strength colors
 # Greater than -69
-GREAT="42f462"
+GREAT="#42f462"
 # -70 to -79
-OKAY="f4a941"
+OKAY="#f4a941"
 # Less than -80
-NOTGOOD="f45241"
+NOTGOOD="#f45241"
 # Other
-OTHER="41a0f4"
+OTHER="#41a0f4"
 
 # Check if NIC exists
 if [[ $(nmcli d show | grep "$NIC") = *GENERAL.DEVICE* ]]; then
@@ -24,11 +24,11 @@ if [[ $(nmcli d show | grep "$NIC") = *GENERAL.DEVICE* ]]; then
     #GetWIFI="-68"
     # Change color based on strength
     if [[ $GetWIFI -ge "-69" ]]; then
-      WIFI="%{F#$GREAT}$GetWIFI dBm%{F-}"
+      WIFI="%{F$GREAT}$GetWIFI dBm%{F-}"
     elif [[ $GetWIFI -ge "-70" && $GetWIFI -le "-79" ]]; then
-      WIFI="%{F#$OKAY}$GetWIFI dBm%{F-}"
+      WIFI="%{F$OKAY}$GetWIFI dBm%{F-}"
     elif [[ $GetWIFI -le "-80" ]]; then
-      WIFI="%{F#$NOTGOOD}$GetWIFI dBm%{F-}"
+      WIFI="%{F$NOTGOOD}$GetWIFI dBm%{F-}"
     else
       # Something is weird, default to blue
       WIFI="%{F#$OTHER}$GetWIFI dBm%{F-}"
